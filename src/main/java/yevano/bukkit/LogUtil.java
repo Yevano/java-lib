c22df748eafad11135feef2c1f5ef8084544e438
James Kingm <jamking1996@gmail.com>
2021-07-14 18:10:36 -0400

    Log!

---
+++ src/main/java/yevano/bukkit/LogUtil.java
@@ -0,0 +1,25 @@
+package yevano.bukkit;
+
+import org.bukkit.Bukkit;
+
+import yevano.util.Format;
+
+public class LogUtil {
+    public interface Context {
+        default void log(String string) {
+            LogUtil.log(string);
+        }
+
+        default void logf(String format, Object... args) {
+            LogUtil.logf(format, args);
+        }
+    }
+
+    public static void log(String string) {
+        Bukkit.getLogger().info(string);
+    }
+
+    public static void logf(String format, Object... args) {
+        log(Format.string(format, args));
+    }
+}
